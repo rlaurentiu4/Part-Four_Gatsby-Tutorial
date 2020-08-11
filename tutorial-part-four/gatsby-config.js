f61789722a404efdeb8ebd565886f2d6a9f1d83f
LaurentiuBadsi <50487193+LaurentiuBadsi@users.noreply.github.com>
2020-08-11 16:16:05 +0300

    Gatsby plugin for manifest
    favicon added in: src/assets/icon.png
    
    Manifest plugin added with basic configuration in the file gatsby-config.js

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -10,6 +10,20 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-transformer-remark`, // highlight-line
     `gatsby-plugin-emotion`,
     {
